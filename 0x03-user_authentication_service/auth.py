73ab258b33b3c818c090eedd6a2a7663ecf8c55b
ogordiunor uche jude <excelxz78@gmail.com>
2024-08-13 15:43:25 +0000

    0x03-user_authentication_service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,18 +1,14 @@
-class Auth:
-    def reset_token(self, email: str) -> str:
-        """
-        Generates a reset_token UUID for a user identified by the given email.
-        
+
+test a reset_token uuid for a user identified by the given email
         Args:
-            email (str): User's email address.
-            
+            email (str): user's email address
         Return:
-            str: Newly generated reset_token for the relevant user.
+            newly generated reset_token for the relevant user
         """
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError("No user found with the given email address.")
+            raise ValueError
 
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
@@ -20,20 +16,17 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """
-        Updates a user's password.
-        
+        Updates a user's password
         Args:
-            reset_token (str): Reset_token issued to reset the password.
-            password (str): User's new password.
-            
+            reset_token (str): reset_token issued to reset the password
+            password (str): user's new password
         Return:
             None
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError("Invalid reset token.")
+            raise ValueError()
 
         hashed = _hash_password(password)
         self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
-
